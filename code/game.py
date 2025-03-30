6c7019a723803a3b4a6c8ab45bb64abf9fa9d0b5
Hevellin <hevellingoncalves@hotmail.com>
2025-03-29 21:24:47 -0300

    creating asset folder inserting all assets creating the menu: background, sound and fonts creating the const file

--- code/game.py
+++ code/game.py
@@ -2,24 +2,19 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
-        # Base do codigo para iniciar a criação do jogo, que é a janela de abertura do jogo
         pygame.init()  # inicializar os modulos da biblioteca
-        self.window = pygame.display.set_mode(size=(600, 480))  # pixels da imagem
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))  # pixels da imagem
 
     def run(self):
+
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-        # check for all events (verificar todos os eventos)
-            # for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:  # event QUIT is close, close the window
-            #        pygame.quit()  # close window
-            #        quit()  # end pygame
-                    # finalização do codigo da janela de abertura do jogo
